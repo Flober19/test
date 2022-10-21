63033496bc3fdef8f2f830cb999ee0f3a4aacd51
Flober19 <116366351+Flober19@users.noreply.github.com>
2022-10-21 16:16:32 +0200

    Update v_tail.m

--- v_tail.m
+++ v_tail.m
@@ -1,6 +1,6 @@
-%% Données
+%% DonnÃ©es
 AR= 7;
-SW = 10.76; %m²
+SW = 10.76; %mÂ²
 bW = 8.7; %m
 c_root = 1.9; %m
 c_tip = 0.57; %m
@@ -9,10 +9,12 @@ cHT_init = 0.8; %between 0.5-1
 LVT = 1; %vertical distance [m]
 LHT = 8; %horizontal distance [m]
 
+%%blabla
+
 %% Calculations
 fun = @(x) (c_root + (c_tip-c_root)/bW*2*x).^2;
 CW_bar = 2/SW * integral(fun,0,bW/2);
 SVT_init = cVT_init*bW*SW/LVT;
 SHT_init = cHT_init*CW_bar*SW/LHT;
 dihedral_angle_init = atan(sqrt(SVT_init/SHT_init));
-fprintf("L'angle initial dihedre est de %d degrés\n", dihedral_angle_init*180/2/pi)
+fprintf("L'angle initial dihedre est de %d degrÃ©s\n", dihedral_angle_init*180/2/pi)
